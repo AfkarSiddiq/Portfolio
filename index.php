f1d815c7988b6bb027d10cc1ad4229fd942fdb0b
afkar <afkarsiddiq0@gmail.com>
2023-04-11 06:33:52 +0700

    little adding

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+
+
+<?php
+    
+    include_once 'header.php';
+    include_once 'home.php';
+    include_once 'about.php';
+    include_once 'resume.php';
+    include_once 'portfolio.php';
+    include_once 'skill.php';
+    
+    ?>
+
+</html>
